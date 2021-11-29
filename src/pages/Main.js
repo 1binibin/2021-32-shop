c3d67a8ebc4b63badfffc2a8dfe69a27fe0f8793
1binibin <namwonbin@gmail.com>
2021-11-29 11:27:11 +0900

    NaviWrap / MyPage

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,16 +2,16 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import HeaderCp from '../components/inc/HeaderCp';
-import TestNavi from '../components/TestNavi';
+// import TestNavi from '../components/TestNavi';
 
 const Wrapper = styled.div`
   max-width: 1200px;
   margin: auto;
 `;
+
 const Main = () => {
   return (
     <Wrapper>
-      <TestNavi />
       <HeaderCp />
       <h1>Main</h1>
     </Wrapper>
