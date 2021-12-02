f08d503184f7f1d04f7316ae4211f24c6d968c4b
1binibin <namwonbin@gmail.com>
2021-12-02 09:34:50 +0900

    복구 복구

--- src/components/inc/HeaderCp.js
+++ src/components/inc/HeaderCp.js
@@ -3,20 +3,20 @@ import { useDispatch } from 'react-redux';
 
 import styled from '../../style';
 import { getAllTree } from '../../store/reducers/tree-slice';
-
+import LogoCp from '../common/LogoCp';
 import NaviWrapCp from './NaviWrapCp';
 import MyPageCp from './MyPageCp';
-import LogoCp from './../common/LogoCp';
 
 const Wrapper = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: relative;
+  z-index: 999;
 `;
 
 const HeaderCp = () => {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getAllTree());
   }, [dispatch]);
--- src/components/inc/SubAllCp.js
+++ src/components/inc/SubAllCp.js
@@ -1,25 +1,22 @@
 import React from 'react';
 
-import styled, { Container, SmallContainer, Underline } from '../../style';
-import ImageCp from './../common/ImageCp';
-import SubNaviCp from './SubNaviCp';
+import styled, { Container, SmallContainer, AnimationWrapper } from '../../style';
+import ImageCp from '../common/ImageCp';
+import SubAllNaviCp from './SubNaviCp';
 
-const TitleLink = styled(Underline)`
-  width: 120px;
-`;
-
-const Wrapper = styled.div`
+const Wrapper = styled(AnimationWrapper)`
   width: 100%;
   border-top: 1px solid #cccccc;
+  border-bottom: 1px solid #cccccc;
   position: absolute;
-  top: 3em;
   left: 0;
-  background-color: burlywood;
+  top: 3em;
+  background-color: #fff;
 `;
 
 const Wrap = styled(SmallContainer)`
-  padding-top: 1.5em;
-  padding-bottom: 1.5em;
+  padding-top: 2.5em;
+  padding-bottom: 2.5em;
   display: flex;
   > :nth-of-type(1) {
     width: 60%;
@@ -41,7 +38,7 @@ const SubAllCp = ({ data }) => {
         <Wrap>
           <SubWrap>
             {data.map((v, i) => (
-              <SubNaviCp data={v} key={i} />
+              <SubAllNaviCp data={v} key={i} />
             ))}
           </SubWrap>
           <div>
